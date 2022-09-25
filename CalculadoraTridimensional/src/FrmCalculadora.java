59f22a8c8661ccde06705c5e376eefe88ecd5ffb
cpalaciosvSOEN <cpalaciosv@soen.pe>
2022-09-24 19:20:56 -0500

    feat(programaDonacionTemplate): Se creó el programaDonación a modo de Plantilla.

---
+++ CalculadoraTridimensional/src/FrmCalculadora.java
@@ -0,0 +1,310 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+
+public class FrmCalculadora extends JFrame implements ActionListener {
+
+	private JPanel contentPane;
+	private JLabel lblCilindro;
+	private JLabel lblRadioCilindro;
+	private JLabel lblAlturaCilindro;
+	private JLabel lblGeneratrizCono;
+	private JLabel lblRadioCono;
+	private JLabel lblCono;
+	private JLabel lblRadioEsfera;
+	private JLabel lblEsfera;
+	private JButton btnCalcularCilindro;
+	private JButton btnCalcularCono;
+	private JButton btnCalcularEsfera;
+	private JTextField txtRadioCilindro;
+	private JTextField txtAlturaCilindro;
+	private JTextField txtRadioCono;
+	private JTextField txtGeneratrizCono;
+	private JTextField txtRadioEsfera;
+	private JSeparator separator;
+	private JSeparator separator_1;
+	private JButton btnLimpiarCilindro;
+	private JButton btnLimpiarCono;
+	private JButton btnLimpiarEsfera;
+	private JTextArea txtSCilindro;
+	private JTextArea txtSCono;
+	private JTextArea txtSEsfera;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmCalculadora frame = new FrmCalculadora();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmCalculadora() {
+		setResizable(false);
+		setTitle("Calculadora de Objetos");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 640, 455);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblCilindro = new JLabel("Cilindro");
+		lblCilindro.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblCilindro.setBounds(10, 11, 117, 19);
+		contentPane.add(lblCilindro);
+		
+		lblRadioCilindro = new JLabel("Radio");
+		lblRadioCilindro.setBounds(10, 41, 46, 14);
+		contentPane.add(lblRadioCilindro);
+		
+		lblAlturaCilindro = new JLabel("Altura");
+		lblAlturaCilindro.setBounds(10, 90, 46, 14);
+		contentPane.add(lblAlturaCilindro);
+		
+		lblGeneratrizCono = new JLabel("Generatriz");
+		lblGeneratrizCono.setBounds(220, 90, 70, 14);
+		contentPane.add(lblGeneratrizCono);
+		
+		lblRadioCono = new JLabel("Radio");
+		lblRadioCono.setBounds(220, 41, 46, 14);
+		contentPane.add(lblRadioCono);
+		
+		lblCono = new JLabel("Cono");
+		lblCono.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblCono.setBounds(220, 11, 117, 19);
+		contentPane.add(lblCono);
+		
+		lblRadioEsfera = new JLabel("Radio");
+		lblRadioEsfera.setBounds(427, 41, 46, 14);
+		contentPane.add(lblRadioEsfera);
+		
+		lblEsfera = new JLabel("Esfera");
+		lblEsfera.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblEsfera.setBounds(427, 11, 117, 19);
+		contentPane.add(lblEsfera);
+		
+		btnCalcularCilindro = new JButton("Calcular");
+		btnCalcularCilindro.addActionListener(this);
+		btnCalcularCilindro.setBounds(7, 156, 89, 23);
+		contentPane.add(btnCalcularCilindro);
+		
+		btnCalcularCono = new JButton("Calcular");
+		btnCalcularCono.addActionListener(this);
+		btnCalcularCono.setBounds(217, 156, 89, 23);
+		contentPane.add(btnCalcularCono);
+		
+		btnCalcularEsfera = new JButton("Calcular");
+		btnCalcularEsfera.addActionListener(this);
+		btnCalcularEsfera.setBounds(427, 156, 89, 23);
+		contentPane.add(btnCalcularEsfera);
+		
+		txtRadioCilindro = new JTextField();
+		txtRadioCilindro.setBounds(10, 55, 185, 20);
+		contentPane.add(txtRadioCilindro);
+		txtRadioCilindro.setColumns(10);
+		
+		txtAlturaCilindro = new JTextField();
+		txtAlturaCilindro.setColumns(10);
+		txtAlturaCilindro.setBounds(10, 104, 185, 20);
+		contentPane.add(txtAlturaCilindro);
+		
+		txtRadioCono = new JTextField();
+		txtRadioCono.setColumns(10);
+		txtRadioCono.setBounds(220, 55, 185, 20);
+		contentPane.add(txtRadioCono);
+		
+		txtGeneratrizCono = new JTextField();
+		txtGeneratrizCono.setColumns(10);
+		txtGeneratrizCono.setBounds(220, 104, 185, 20);
+		contentPane.add(txtGeneratrizCono);
+		
+		txtRadioEsfera = new JTextField();
+		txtRadioEsfera.setColumns(10);
+		txtRadioEsfera.setBounds(427, 55, 188, 20);
+		contentPane.add(txtRadioEsfera);
+		
+		separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setBackground(new Color(192, 192, 192));
+		separator.setForeground(new Color(192, 192, 192));
+		separator.setBounds(205, 11, 2, 383);
+		contentPane.add(separator);
+		
+		separator_1 = new JSeparator();
+		separator_1.setOrientation(SwingConstants.VERTICAL);
+		separator_1.setForeground(new Color(192, 192, 192));
+		separator_1.setBackground(new Color(192, 192, 192));
+		separator_1.setBounds(415, 11, 2, 383);
+		contentPane.add(separator_1);
+		
+		btnLimpiarCilindro = new JButton("Limpiar");
+		btnLimpiarCilindro.addActionListener(this);
+		btnLimpiarCilindro.setBounds(106, 156, 89, 23);
+		contentPane.add(btnLimpiarCilindro);
+		
+		btnLimpiarCono = new JButton("Limpiar");
+		btnLimpiarCono.addActionListener(this);
+		btnLimpiarCono.setBounds(316, 156, 89, 23);
+		contentPane.add(btnLimpiarCono);
+		
+		btnLimpiarEsfera = new JButton("Limpiar");
+		btnLimpiarEsfera.addActionListener(this);
+		btnLimpiarEsfera.setBounds(526, 156, 89, 23);
+		contentPane.add(btnLimpiarEsfera);
+		
+		txtSCilindro = new JTextArea();
+		txtSCilindro.setBounds(10, 190, 185, 204);
+		contentPane.add(txtSCilindro);
+		
+		txtSCono = new JTextArea();
+		txtSCono.setBounds(217, 190, 188, 204);
+		contentPane.add(txtSCono);
+		
+		txtSEsfera = new JTextArea();
+		txtSEsfera.setBounds(427, 190, 188, 204);
+		contentPane.add(txtSEsfera);
+	}
+	
+	// Section Actions
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnLimpiarEsfera) {
+			actionPerformedBtnLimpiarEsfera(e);
+		}
+		if (e.getSource() == btnCalcularEsfera) {
+			actionPerformedBtnCalcularEsfera(e);
+		}
+		if (e.getSource() == btnLimpiarCono) {
+			actionPerformedBtnLimpiarCono(e);
+		}
+		if (e.getSource() == btnCalcularCono) {
+			actionPerformedBtnCalcularCono(e);
+		}
+		if (e.getSource() == btnLimpiarCilindro) {
+			actionPerformedBtnLimpiarCilindro(e);
+		}
+		if (e.getSource() == btnCalcularCilindro) {
+			actionPerformedBtnCalcularCilindro(e);
+		}
+	}
+	// END Section Actions
+	
+	//Section Calculate Cylinder
+	protected void actionPerformedBtnCalcularCilindro(ActionEvent e) {
+		
+		//Declaration variable
+		double areaLateral, areaTotal, areaBase, radio, altura;
+		
+		//Parsing values
+		DecimalFormat formato = new DecimalFormat("#.##");
+		radio = Double.parseDouble(txtRadioCilindro.getText());
+		altura = Double.parseDouble(txtAlturaCilindro.getText());
+		
+		//Process
+		areaLateral = 2 * 3.1416 * radio * altura;
+		areaBase = 3.1416 * (radio * radio);
+		areaTotal = 2 * (areaBase + areaLateral);
+		
+		//Output
+		txtSCilindro.setText("Especificaciones Cilindro \n\n");
+		txtSCilindro.append("Área Lateral:\n" + formato.format(areaLateral) + "\n\n");
+		txtSCilindro.append("Área Base:\n" + formato.format(areaBase) + "\n\n");
+		txtSCilindro.append("Área Total:\n" + formato.format(areaTotal));
+		
+	}
+	protected void actionPerformedBtnLimpiarCilindro(ActionEvent e) {
+		
+		txtRadioCilindro.setText("");
+		txtAlturaCilindro.setText("");
+		txtSCilindro.setText("");
+		txtRadioCilindro.requestFocus();
+		
+	}
+	//END Section Calculate Cylinder
+	
+	//Section Calculate Cone
+	protected void actionPerformedBtnCalcularCono(ActionEvent e) {
+		
+		//Declaration variable
+		double areaBase, perimetroBase, areaLateral, radio, generatriz;
+		
+		//Parsing values
+		DecimalFormat formato = new DecimalFormat("#.##");
+		radio = Double.parseDouble(txtRadioCono.getText());
+		generatriz = Double.parseDouble(txtGeneratrizCono.getText());
+		
+		//Process
+		areaBase = 3.14 * (radio * radio);
+		perimetroBase = (3.14 * 2) * radio;
+		areaLateral = (perimetroBase * generatriz) / 2;
+		
+		//Output
+		txtSCono.setText("Especificaciones Cono \n\n");
+		txtSCono.append("Área Base:\n" + formato.format(areaBase) + "\n\n");
+		txtSCono.append("Perímetro Base:\n" + formato.format(perimetroBase) + "\n\n");
+		txtSCono.append("Área Lateral:\n" + formato.format(areaLateral));
+		
+	}
+	protected void actionPerformedBtnLimpiarCono(ActionEvent e) {
+		
+		txtRadioCono.setText("");
+		txtGeneratrizCono.setText("");
+		txtSCono.setText("");
+		txtRadioCono.requestFocus();
+		
+	}
+	//END Section Calculate Cone
+	
+	//Section Calculate Sphere
+	protected void actionPerformedBtnCalcularEsfera(ActionEvent e) {
+		
+		//Declaration variable
+		double area, volumen, radio;
+		
+		//Parsing values
+		DecimalFormat formato = new DecimalFormat("#.##");
+		radio = Double.parseDouble(txtRadioEsfera.getText());
+				
+		//Process
+		area = 12.57 * (radio * radio);
+		volumen = (12.57 * (radio * radio * radio)) / 3;
+		
+		//Output
+		txtSEsfera.setText("Especificaciones Esfera \n\n");
+		txtSEsfera.append("Área:\n" + formato.format(area) + "\n\n");
+		txtSEsfera.append("Volumen:\n" + formato.format(volumen));
+		
+	}
+	protected void actionPerformedBtnLimpiarEsfera(ActionEvent e) {
+		txtRadioEsfera.setText("");
+		txtSEsfera.setText("");
+		txtRadioEsfera.requestFocus();
+	}
+	// END Section Calculate Sphere
+	
+}
---
+++ programaDonativoInstitucion/src/programaInstitucion/FormProgramaInstitucion.java
@@ -0,0 +1,147 @@
+package programaInstitucion;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+public class FormProgramaInstitucion extends JFrame implements ActionListener {
+
+	private JPanel contentPane;
+	private JLabel lblTitulo;
+	private JLabel lblSubtitulo;
+	private JLabel lblDonacion;
+	private JSeparator separator;
+	private JTextField txtDonacion;
+	private JButton btnDonar;
+	private JButton btnLimpiar;
+	private JTextArea txtSDonacion;
+	private JScrollPane scrollPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FormProgramaInstitucion frame = new FormProgramaInstitucion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FormProgramaInstitucion() {
+		setTitle("Programa de Donaciones");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 550, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblTitulo = new JLabel("Programa de Donaciones");
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblTitulo.setBounds(10, 11, 514, 34);
+		contentPane.add(lblTitulo);
+		
+		lblSubtitulo = new JLabel("Gracias por su donaci\u00F3n buen samaritano");
+		lblSubtitulo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblSubtitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSubtitulo.setBounds(10, 56, 514, 26);
+		contentPane.add(lblSubtitulo);
+		
+		lblDonacion = new JLabel("Donaci\u00F3n:");
+		lblDonacion.setBounds(10, 113, 61, 14);
+		contentPane.add(lblDonacion);
+		
+		separator = new JSeparator();
+		separator.setForeground(Color.LIGHT_GRAY);
+		separator.setBackground(Color.LIGHT_GRAY);
+		separator.setBounds(10, 93, 514, 2);
+		contentPane.add(separator);
+		
+		txtDonacion = new JTextField();
+		txtDonacion.setBounds(76, 110, 114, 20);
+		contentPane.add(txtDonacion);
+		txtDonacion.setColumns(10);
+		
+		btnDonar = new JButton("Donar");
+		btnDonar.addActionListener(this);
+		btnDonar.setBounds(200, 109, 89, 23);
+		contentPane.add(btnDonar);
+		
+		btnLimpiar = new JButton("Limpiar");
+		btnLimpiar.addActionListener(this);
+		btnLimpiar.setBounds(435, 109, 89, 23);
+		contentPane.add(btnLimpiar);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(10, 138, 514, 212);
+		contentPane.add(scrollPane);
+		
+		txtSDonacion = new JTextArea();
+		scrollPane.setViewportView(txtSDonacion);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnLimpiar) {
+			actionPerformedBtnLimpiar(e);
+		}
+		if (e.getSource() == btnDonar) {
+			actionPerformedBtnDonar(e);
+		}
+	}
+	protected void actionPerformedBtnDonar(ActionEvent e) {
+		//Declaration variable
+		double hospital, comedor, guarderia, escuela, asilo, donacion;
+		
+		//Parsing values
+		DecimalFormat formato = new DecimalFormat("#.##");
+		donacion = Double.parseDouble(txtDonacion.getText());
+		
+		//Process
+		hospital = 0.25 * donacion;
+		guarderia = 0.15 * donacion;
+		escuela = 0.65 * (hospital + guarderia);
+		comedor = 0.45 * escuela;
+		asilo = donacion - (hospital + guarderia + escuela - comedor);
+		
+		//Output
+		txtSDonacion.setText("¿Cómo vamos a repartir nuestros donativos? \n");
+		txtSDonacion.append("De la siguiente manera: \n\n");
+		txtSDonacion.append("Hospital:\n" + formato.format(hospital) + "\n\n");
+		txtSDonacion.append("Comedor:\n" + formato.format(comedor) + "\n\n");
+		txtSDonacion.append("Guardería:\n" + formato.format(guarderia) + "\n\n");
+		txtSDonacion.append("Escuela:\n" + formato.format(escuela) + "\n\n");
+		txtSDonacion.append("Asilo:\n" + formato.format(asilo));
+	}
+	protected void actionPerformedBtnLimpiar(ActionEvent e) {
+		txtDonacion.setText("");
+		txtSDonacion.setText("");
+		txtDonacion.requestFocus();
+	}
+}
